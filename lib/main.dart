84388d477ab537c1355ea7665472464664882a0a
porschier <marfoporschier@gmail.com>
2021-12-04 14:42:58 +0000

    passed and added bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class HomeView extends StatelessWidget {
         leading:  Center(child: CircleAvatar(radius: 25, backgroundImage: NetworkImage("https://image.shutterstock.com/image-photo/poznan-pol-jan-6-2021-600w-1924116089.jpg"))), 
 
         title: Text("My task"),
-        actions: [
+        actions: const [
             
          Icon(Icons.search), Icon(Icons.filter_list),
           
@@ -41,25 +41,40 @@ class HomeView extends StatelessWidget {
        padding: const EdgeInsets.all(8.0),
        child: ListView(
     
-         children: [ToDoWidget(),
-       Card(elevation: 15,
-        child: 
-         ListTile(
-           leading: Icon(Icons.check_box),
-           title: Text("Plan the trip to Finland"),
-           subtitle: Text("the family's trip to finland next summer"),
-       trailing:  Text("yesterday"),
-           ),
+         children: const [ToDoWidget(),
+       Padding(
+         padding: EdgeInsets.all(8.0),
+         child: Card(elevation: 15,
+          child: 
+           ListTile(
+             leading: Icon(Icons.check_box),
+             title: Text("Plan the trip to Finland"),
+             subtitle: Text("the family's trip to finland next summer"),
+         trailing:  Text("yesterday"),
+             ),
   
-         ),ToDoWidget(),
+           ),
+       ),ToDoWidget(),
          ToDoWidget(),
          ToDoWidget(),
          ToDoWidget(),
          ToDoWidget(),
+          ToDoWidget(),
+           ToDoWidget(), ToDoWidget(),
+        
        ]),
      ),
-      
-       );
+      floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
+
+      bottomSheet:Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card( child:Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(children: const [Icon(Icons.check_circle),SizedBox(width: 5,), Text("Completed"), Icon(Icons.arrow_drop_down ), Spacer(), Text("24")] ),
+        ),
+        color: Colors.blue[50]),
+      ));
+       
        
      
     
